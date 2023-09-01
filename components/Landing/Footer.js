c3077a17f1135862f2fcbb65ea95186d83430f46
Mohid Meer <mohidfauji@gmail.com>
2023-09-01 14:54:32 +0500

    updated

--- components/Landing/Footer.js
+++ components/Landing/Footer.js
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Subtitle from "../Commons/Subtitle";
-import fimg from '@/public/images/Commercial.jpg'
+import fimg from '@/public/images/commercial.jpg'
 
 export default function Footer(){
     return (
@@ -17,9 +17,9 @@ export default function Footer(){
             <div className="flex flex-col  justify-center">
                 <div className="mx-auto max-w-5xl flex lg:flex-row flex-col justify-between">
                     <div className="text-white mt-32 ">
-                        <Subtitle text={'Herts Roofing & Construction'}/>
+                        <Subtitle text={'Host Building Group & Construction'}/>
                         <h2 className="text-6xl font-bold text-white">Our <span className=" red-gradient px-2">Promise</span></h2>
-                        <p className="w-2/3 mt-10 text-lg">At Herts Roofing & Construction, our mission is to provide the highest level of professionalism and customer service to all of our customers. We stay on budget, and complete projects on time.</p>
+                        <p className="w-2/3 mt-10 text-lg">At Host Building Group & Construction, our mission is to provide the highest level of professionalism and customer service to all of our customers. We stay on budget, and complete projects on time.</p>
                     </div>
                     <form className="flex-col  w-full bg-white p-10 rounded-2xl lg:mt-8 mt-20">
                         <p className="text-3xl font-bold my-8">Drop a Line</p>
