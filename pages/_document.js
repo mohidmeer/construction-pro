b6a986bcd01559988339d26f2b03513db5d5e901
Mohid Meer <mohidfauji@gmail.com>
2024-05-04 07:10:47 +0500

    updated

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head >
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="manifest" href="/site.webmanifest" />
+      </Head>
       <body>
         <Main />
         <NextScript />
